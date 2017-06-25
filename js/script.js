6e604d404612f10cb8ed7902a806d7a38140c66d
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-06-25 19:53:59 +0200

    creating object canvas in html DOM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var canvasArea = document.createElement("canvas");
+    canvasArea.width = 500;
+    canvasArea.height = 500;
+    document.body.appendChild(canvasArea);
+})
