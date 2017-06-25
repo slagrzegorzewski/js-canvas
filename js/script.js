ae82d759f6f88b37f09249f5bf95a43b293d8d22
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-06-25 21:07:52 +0200

    using strict and first draw in canvas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
     var canvasArea = document.createElement("canvas");
     canvasArea.width = 500;
     canvasArea.height = 500;
     document.body.appendChild(canvasArea);
-})
+
+    var ctx = canvasArea.getContext("2d");
+
+    ctx.fillRect(100, 50, 250, 50);
+});
